d00c2df0740f39353c3ae758e13f9f603d7a96fb
ryanmrichard <ryanmrichard1@gmail.com>
2017-05-31 17:59:10 -0500

    Much clean-up

--- SimintERI.cpp
+++ SimintERI.cpp
@@ -4,24 +4,22 @@
 
 #include "SimintERI.hpp"
 
-#include "simint/simint_init.h"
-#include "simint/eri/eri.h"
+#include "simint/simint.h"
 
+using namespace pulsar;
 
-using namespace pulsar::output;
-using namespace pulsar::exception;
-using namespace pulsar::system;
-using namespace pulsar::datastore;
-
-
+//This is what gaussian_shell is replaced by
+using SimintShell_t=simint_shell;
+//This is what multishell_pair is replaced by
+using SimintPair=simint_multi_shellpair;
 
 static
-gaussian_shell ToSimintShell(const BasisSetShell & shell, int igen)
+SimintShell_t ToSimintShell(const BasisSetShell & shell, int igen)
 {
     size_t nprim = shell.n_primitives();
 
-    gaussian_shell gs;
-    allocate_gaussian_shell(nprim, &gs);
+    SimintShell_t gs;
+    simint_allocate_shell(nprim, &gs);
     gs.nprim = nprim;
     gs.am = shell.general_am(igen);
     gs.x = shell.get_coord(0);
@@ -33,84 +31,98 @@ gaussian_shell ToSimintShell(const BasisSetShell & shell, int igen)
     std::copy(alphas, alphas + nprim, gs.alpha);
     std::copy(coefs, coefs + nprim, gs.coef);
 
-    normalize_gaussian_shells(1, &gs);
+    simint_normalize_shells(1, &gs);
 
     return gs;
 }
 
 
-uint64_t SimintERI::calculate_(size_t shell1, size_t shell2,
-                               size_t shell3, size_t shell4,
-                               double * outbuffer, size_t bufsize)
+double *SimintERI::calculate_(size_t shell1, size_t shell2,
+                               size_t shell3, size_t shell4)
 {
     const BasisSetShell & sh1 = bs1_.shell(shell1);
     const BasisSetShell & sh2 = bs2_.shell(shell2);
     const BasisSetShell & sh3 = bs3_.shell(shell3);
     const BasisSetShell & sh4 = bs4_.shell(shell4);
 
-    size_t nfunc = sh1.n_functions() * sh2.n_functions() * sh3.n_functions() * sh4.n_functions();
-
-    if(bufsize < nfunc)
-        throw GeneralException("Buffer to small for ERI", "bufsize", bufsize, "nfunc", nfunc);
-
     double * srcptr = sourcework_;
 
     for(size_t ng1 = 0; ng1 < sh1.n_general_contractions(); ng1++)
     {
-        gaussian_shell gs1 = ToSimintShell(sh1, ng1);
+        SimintShell_t gs1 = ToSimintShell(sh1, ng1);
         const size_t ncart1 = n_cartesian_gaussian(sh1.general_am(ng1));
 
         for(size_t ng2 = 0; ng2 < sh2.n_general_contractions(); ng2++)
         {
-            gaussian_shell gs2 = ToSimintShell(sh2, ng2);
+            SimintShell_t gs2 = ToSimintShell(sh2, ng2);
             const size_t ncart2 = n_cartesian_gaussian(sh2.general_am(ng2));
-            multishell_pair bra_pair = create_multishell_pair(1, &gs1, 1, &gs2); 
+            SimintPair bra_pair;
+            simint_initialize_multi_shellpair(&bra_pair);
+            simint_create_multi_shellpair(1, &gs1, 1, &gs2, &bra_pair, 0);
 
             for(size_t ng3 = 0; ng3 < sh3.n_general_contractions(); ng3++)
             {
-                gaussian_shell gs3 = ToSimintShell(sh3, ng3);
+                SimintShell_t gs3 = ToSimintShell(sh3, ng3);
                 const size_t ncart3 = n_cartesian_gaussian(sh3.general_am(ng3));
 
                 for(size_t ng4 = 0; ng4 < sh4.n_general_contractions(); ng4++)
                 {
                     // form the shell pair info
-                    gaussian_shell gs4 = ToSimintShell(sh4, ng4);
-                    multishell_pair ket_pair = create_multishell_pair(1, &gs3, 1, &gs4); 
+                    SimintShell_t gs4 = ToSimintShell(sh4, ng4);
+                    SimintPair ket_pair;
+                    simint_initialize_multi_shellpair(&ket_pair);
+                    simint_create_multi_shellpair(1, &gs3, 1, &gs4,&ket_pair,0);
                     const size_t ncart4 = n_cartesian_gaussian(sh4.general_am(ng4));
                     const size_t ncart = ncart1 * ncart2 * ncart3 * ncart4;
 
-                    size_t ncalc = simint_compute_eri(bra_pair, ket_pair, srcptr);
+                    size_t ncalc = simint_compute_eri(&bra_pair, &ket_pair, 0.0, srcptr);
 
-                    free_gaussian_shell(gs4);
-                    free_multishell_pair(ket_pair);
+                    simint_free_shell(&gs4);
+                    simint_free_multi_shellpair(&ket_pair);
 
                     srcptr += ncalc * ncart;
                 }
 
-                free_gaussian_shell(gs3);
+                simint_free_shell(&gs3);
             }
 
-            free_gaussian_shell(gs2);
-            free_multishell_pair(bra_pair);
+            simint_free_shell(&gs2);
+            simint_free_multi_shellpair(&bra_pair);
         }
 
-        free_gaussian_shell(gs1);
+        simint_free_shell(&gs1);
     }
 
 
-    CartesianToSpherical_4Center(sh1, sh2, sh3, sh4, sourcework_, outbuffer, transformwork_, 1);
+    CartesianToSpherical_4Center(sh1, sh2, sh3, sh4, sourcework_, buffer_.data(), transformwork_, 1);
 
-    return nfunc;
+    return buffer_.data();
+}
+
+//Returns 3 multichoose l, i.e. the number of Cartesian Gaussians for angular
+//momentum l
+static size_t multichoose(size_t l)
+{
+    if(l==0)return 1;
+    else if(l==1)return 3;
+    else if(l==2)return 6;
+    else if(l==3)return 10;
+    else if(l==4)return 15;
+    else if(l==5)return 21;
+    else if(l==6)return 28;
+    else if(l==7)return 36;
+    else
+      throw PulsarException("Simint only supports up to angular momentum l==7");
 }
 
 
 void SimintERI::initialize_(unsigned int deriv,
-                            const Wavefunction & wfn,
+                            const Wavefunction &,
                             const BasisSet & bs1, const BasisSet & bs2,
                             const BasisSet & bs3, const BasisSet & bs4)
 {
     if(deriv != 0)
-        throw NotYetImplementedException("Not Yet Implemented: Simint with deriv != 0");
+        throw PulsarException("Not Yet Implemented: Simint with deriv != 0");
 
     // initialize the simint library
     simint_init();
@@ -122,7 +134,6 @@ void SimintERI::initialize_(unsigned int deriv,
     bs3_ = bs3;
     bs4_ = bs4;
 
-
     // determine work sizes
     size_t maxsize1 = bs1_.max_property(n_cartesian_gaussian_for_shell_am);
     size_t maxsize2 = bs2_.max_property(n_cartesian_gaussian_for_shell_am);
@@ -130,14 +141,15 @@ void SimintERI::initialize_(unsigned int deriv,
     size_t maxsize4 = bs4_.max_property(n_cartesian_gaussian_for_shell_am);
     size_t transformwork_size = maxsize1*maxsize2*maxsize3*maxsize4;
     
-    maxsize1 =  bs1_.max_property(n_cartesian_gaussian_in_shell);
-    maxsize2 =  bs2_.max_property(n_cartesian_gaussian_in_shell);
-    maxsize3 =  bs3_.max_property(n_cartesian_gaussian_in_shell);
-    maxsize4 =  bs4_.max_property(n_cartesian_gaussian_in_shell);
+    maxsize1 =  multichoose(*bs1_.all_am().rbegin());
+    maxsize2 =  multichoose(*bs2_.all_am().rbegin());
+    maxsize3 =  multichoose(*bs3_.all_am().rbegin());
+    maxsize4 =  multichoose(*bs4_.all_am().rbegin());
     size_t sourcework_size = maxsize1*maxsize2*maxsize3*maxsize4;
 
     work_.resize(sourcework_size+transformwork_size);
     sourcework_ = work_.data();
-    transformwork_ = sourcework_ + sourcework_size;   
+    transformwork_ = sourcework_ + sourcework_size;
+    buffer_.resize(transformwork_size);
 }
 
