44c51650cb39027313f7a9f206d7dcd16922639d
ryanmrichard <ryanmrichard1@gmail.com>
2017-05-31 18:17:40 -0500

    Adds missing header file

--- SimintERI.hpp
+++ SimintERI.hpp
@@ -1,29 +1,41 @@
 #ifndef _GUARD_SIMINTRI_HPP_
 #define _GUARD_SIMINTRI_HPP_
 
-#include <pulsar/modulebase/TwoElectronIntegral.hpp>
+#include <pulsar/modulebase/FourCenterIntegral.hpp>
 #include <pulsar/system/BasisSet.hpp>
 
-class SimintERI : public pulsar::modulebase::TwoElectronIntegral
+class SimintERI : public pulsar::FourCenterIntegral
 {
 public:
-    using pulsar::modulebase::TwoElectronIntegral::TwoElectronIntegral;
+    using pulsar::FourCenterIntegral::FourCenterIntegral;
+
+    HashType my_hash_(unsigned int deriv,
+                      const pulsar::Wavefunction&,
+                      const pulsar::BasisSet& bs1,
+                      const pulsar::BasisSet& bs2,
+                      const pulsar::BasisSet& bs3,
+                      const pulsar::BasisSet& bs4)
+    {
+        return bphash::hash_to_string(bs1.my_hash())+
+                bphash::hash_to_string(bs2.my_hash())+
+                bphash::hash_to_string(bs3.my_hash())+
+                bphash::hash_to_string(bs4.my_hash())+
+                std::to_string(deriv);
+    }
 
     virtual void initialize_(unsigned int deriv,
-                             const pulsar::datastore::Wavefunction & wfn,
-                             const pulsar::system::BasisSet & bs1,
-                             const pulsar::system::BasisSet & bs2,
-                             const pulsar::system::BasisSet & bs3,
-                             const pulsar::system::BasisSet & bs4);
+                             const pulsar::Wavefunction & wfn,
+                             const pulsar::BasisSet & bs1,
+                             const pulsar::BasisSet & bs2,
+                             const pulsar::BasisSet & bs3,
+                             const pulsar::BasisSet & bs4);
 
-
-    virtual uint64_t calculate_(size_t shell1, size_t shell2,
-                                size_t shell3, size_t shell4,
-                                double * outbuffer, size_t bufsize);
+    virtual double* calculate_(size_t shell1, size_t shell2,
+                               size_t shell3, size_t shell4);
 
 private:
-    pulsar::system::BasisSet bs1_, bs2_, bs3_, bs4_;
-
+    pulsar::BasisSet bs1_, bs2_, bs3_, bs4_;
+    std::vector<double> buffer_;
     std::vector<double> work_;
     double * sourcework_;
     double * transformwork_;
